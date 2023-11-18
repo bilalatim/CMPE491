5e3f24465d3019486cda6a994d127692eae339ec
Bilal <bilalatim27@gmail.com>
2023-11-18 14:06:25 +0300

    Add Unity Navmesh Demo Project

---
+++ Unity Projects/NavMesh/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent navMeshAgent;
+    public ThirdPersonCharacter character;
+    
+
+    private void Start()
+    {
+        navMeshAgent.updateRotation = false;
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                // navMeshAgent.SetDestination(hit.point);
+                navMeshAgent.destination = hit.point;
+            }
+            else
+            {
+                Debug.DrawRay(ray.origin,ray.direction * 1000 ,Color.red);
+                Debug.Log("Did not Hit");
+            }
+            
+        }
+        if (navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance)
+        {
+            character?.Move(navMeshAgent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character?.Move(Vector3.zero, false, false);
+        }
+    }
+}
